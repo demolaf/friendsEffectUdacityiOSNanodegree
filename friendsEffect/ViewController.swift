02429894e658d71589e830904ebd7fedf6cf1417
Ademola Fadumo <demolafadumo@gmail.com>
2023-03-29 22:51:08 +0100

    Initial Commit

---
+++ friendsEffect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  friendsEffect
+//
+//  Created by Ademola Fadumo on 29/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
